5e4ef8c3cdae429a1a59a70a325fb20f8b01b609
Nithish Raj <112042650+Nithish9345@users.noreply.github.com>
2023-07-19 12:54:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 class ElementRemoval:
     def element(self, array):
         s = 0
-        for i in array:
+        for i in range(len(array)):
             s += sum(array)
             array[i] = 0
 
         return s
 
 array = list(map(int, input().split()))
-array.sort()
+array.sort(reverse=True)
 object = ElementRemoval()
 print(object.element(array))
